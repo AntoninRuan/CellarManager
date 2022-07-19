840c7d99b5678fade774f1fc270d7506c6e6f45a
Antonin Ruan <womax91@gmail.com>
2022-07-19 22:02:03 +0200

    Mise à jour du serveur HTTP qui utilise désormais la classe HttpServer et dont les modifications passe par le méthod POST, PUT et DELETE

---
+++ src/main/java/fr/antoninruan/cellarmanager/utils/mobile_sync/HttpMethod.java
@@ -0,0 +1,16 @@
+package fr.antoninruan.cellarmanager.utils.mobile_sync;
+
+public enum HttpMethod {
+
+    GET,
+    HEAD,
+    POST,
+    PUT,
+    DELETE,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH;
+
+}
+
